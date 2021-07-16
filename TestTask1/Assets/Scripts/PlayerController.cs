74f42eb62d7598d08464273cd9e59888f6f4d6f7
ColosusPX <67974952+ColosusPX@users.noreply.github.com>
2021-07-17 00:04:43 +0400

    Complete Task 1

---
+++ TestTask1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] FixedJoystick _joystick;
+    [SerializeField] float _speed;
+
+    Vector3 _distance;
+
+    void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0) && _joystick.Direction == Vector2.zero)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                _distance = hit.point;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_joystick.Direction != Vector2.zero)
+        {
+            transform.position += new Vector3(_joystick.Horizontal, 0, _joystick.Vertical) * _speed * Time.fixedDeltaTime;
+            _distance = Vector3.zero;
+        }
+        else if(_distance != Vector3.zero && (_distance - transform.position).magnitude > .7f)
+        {
+            transform.position += (_distance - transform.position).normalized * _speed * Time.fixedDeltaTime;
+        }
+
+    }
+}
